81b923c3d3e81e66bf569fe575fc84a5fe6f0157
AndhikaRei <andhikareihan349@gmail.com>
2021-02-16 18:58:26 +0700

    Ilangin banana predik

--- src/main/java/za/co/entelect/challenge/command/BananaCommand.java
+++ src/main/java/za/co/entelect/challenge/command/BananaCommand.java
@@ -2,22 +2,26 @@ package za.co.entelect.challenge.command;
 
 public class BananaCommand implements Command {
 
+    // Attribute
     private final int x;
     private final int y;
     private int selectedWorm;
 
     public BananaCommand(int x, int y) {
+    //  Constructor
         this.x = x;
         this.y = y;
         this.selectedWorm = -1;
     }
 
     public BananaCommand(int x, int y, int select) {
+    //  Constructor user defined
         this.x = x;
         this.y = y;
         this.selectedWorm = select;
     }
 
+    // Render string command untuk dibaca game engine
     @Override
     public String render() {
         if (selectedWorm == -1) {
